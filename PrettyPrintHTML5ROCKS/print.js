f74c053e83b7778d89ef497b7cf6c2e785ff1fd1
Ishan <ishanatmuzaffarpur@gmail.com>
2013-12-02 19:52:59 +0530

    The extension now toggles between print view and web view. Clicking on the extension icon again now brings it back to it's original view.

--- PrettyPrintHTML5ROCKS/print.js
+++ PrettyPrintHTML5ROCKS/print.js
@@ -27,72 +27,145 @@
 if(location.href.substring(0,39) === "http://www.html5rocks.com/en/tutorials/" && location.href.length > 46) {
 	// html5rocks->tutorial
 	
+	// switching between print and web display
+	// defaults to 'false' in the first run. I have no Idea why.
+	// If someone can explain this then please do in the comments.
+	if(document.getElementsByTagName('header')[0].style.display==='none') {
+		pageMode='block';
+		urlMode='none';
+	} else {
+		pageMode='none';
+		urlMode='block';
+	}
+	
 	// Remove top header
-	document.getElementsByTagName('header')[0].style.display='none';
+	document.getElementsByTagName('header')[0].style.display=pageMode;
 
-	// set margin to 1em to give nice spacing
+	// selecting divs so that further elements of the page can be selected
 	var divs=document.getElementsByTagName('div');
-	divs[5].style.margin='1em auto';
+	// switch margin between 4.5em to 1em to give nice spacing
+	if(pageMode=='none') {
+		divs[5].style.margin='1em auto';
+	} else {
+		divs[5].style.margin='4.5em auto';
+	}
 
 	// Select article header
 	var header=divs[5].firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling;
 	// Remove Social Share Buttons
-	header.nextSibling.nextSibling.style.display='none';
+	header.nextSibling.nextSibling.style.display=pageMode;
 	// Remove no. of Comments
-	header.getElementsByTagName('a')[2].style.display='none';
+	header.getElementsByTagName('a')[2].style.display=pageMode;
 
 	// Select the main article wrapper
 	var article=divs[5].firstChild.nextSibling.nextSibling.nextSibling;
 	// Remove the table of contents
-	article.firstChild.nextSibling.style.display='none';
-	// Eliminating the gap created because of removing 'table of conents'
-	article.style.marginLeft='0px';
+	article.firstChild.nextSibling.style.display=pageMode;
+	
+	// Eliminating the gap created because of removing 'table of conents' (switching for filling space with table of contents)
+	if(pageMode=='none') {
+		article.style.marginLeft='0px';
+	} else {
+		article.style.marginLeft='340px';
+	}
 
 	// Selecting reference div
 	var ref=article.nextSibling.nextSibling;
-	// Set innerText to the url
-	ref.innerText=location.href;
-	// Stylising the reference
-	ref.style.marginLeft='0px';
-	ref.style.textAlign='center';
-	ref.style.fontWeight='bold';
+	// switch between print mode and web mode for displaying reference
+	ref.style.display=pageMode;
+	
 	// Removing the comments
-	ref.nextSibling.nextSibling.style.display='none';
+	ref.nextSibling.nextSibling.style.display=pageMode;
+	
+	// Creating URL Holder if it is not deifned
+	if(typeof(urlHolder)=='undefined') {
+		// Create a new element for the URL
+		var urlHolder = document.createElement('section');
+		// make it look like it belongs
+		urlHolder.className='cc pattern-bg-lighter';
+		// append it side by side with reference
+		divs[5].appendChild(urlHolder);
+		// Set innerText to the url
+		urlHolder.innerText=location.href;
+		// Stylising the reference
+		urlHolder.style.marginLeft='0px';
+		urlHolder.style.textAlign='center';
+		urlHolder.style.fontWeight='bold';
+	}
+	
+	// switching display of urlHolder for print mode and web mode
+	urlHolder.style.display=urlMode;
 
 	// Print the document.
-	window.print();
+	if(pageMode=='none') {
+		window.print();
+	}
 	
 } else if(location.href.substring(0,30) === "http://updates.html5rocks.com/" && location.href.length > 30) {
 	// html5rocks->update
 	
+	// switching between print and web display
+	// defaults to 'false' in the first run. I have no Idea why.
+	// If someone can explain this then please do in the comments.
+	if(document.getElementsByTagName('header')[0].style.display==='none') {
+		pageMode='block';
+		urlMode='none';
+	} else {
+		pageMode='none';
+		urlMode='block';
+	}
+	
 	// Remove top header
-	document.getElementsByTagName('header')[0].style.display='none';
-
-	// set margin to 1em to give nice spacing
+	document.getElementsByTagName('header')[0].style.display=pageMode;
+	
+	// selecting divs so that further elements of the page can be selected
 	var divs=document.getElementsByTagName('div');
-	divs[5].style.margin='1em auto';
-
+	// switch margin between 4.5em to 1em to give nice spacing
+	if(pageMode=='none') {
+		divs[5].style.margin='1em auto';
+	} else {
+		divs[5].style.margin='4.5em auto';
+	}
+	
 	// Remove tags, blank space and google+ share
-	divs[7].style.display='none';
-	divs[8].style.display='none';
-	divs[9].style.display='none';
-
+	divs[7].style.display=pageMode;
+	divs[8].style.display=pageMode;
+	divs[9].style.display=pageMode;
+	
 	// Setting reference div
 	var ref=divs[5].firstChild.nextSibling.lastChild.previousSibling;
-	// Set innerText to the url
-	ref.innerText=location.href;
-	// Stylising the reference
-	ref.style.marginLeft='0px';
-	ref.style.textAlign='center';
-	ref.style.fontWeight='bold';
+	
+	// switch between print mode and web mode for displaying reference
+	ref.style.display=pageMode;
+	
+	// Creating URL Holder if it is not deifned
+	if(typeof(urlHolder)=='undefined') {
+		// Create a new element for the URL
+		var urlHolder = document.createElement('section');
+		// make it look like it belongs
+		urlHolder.className='cc pattern-bg-lighter';
+		// append it side by side with reference
+		divs[5].appendChild(urlHolder);
+		// Set innerText to the url
+		urlHolder.innerText=location.href;
+		// Stylising the reference
+		urlHolder.style.marginLeft='0px';
+		urlHolder.style.textAlign='center';
+		urlHolder.style.fontWeight='bold';
+	}
+	
+	// switching display of urlHolder for print mode and web mode
+	urlHolder.style.display=urlMode;
 	
 	// Print the document.
-	window.print();
+	if(pageMode=='none') {
+		window.print();
+	}
 	
 } else if(location.href.substring(0,30) === "http://updates.html5rocks.com/" || location.href.substring(0,29) === "http://www.html5rocks.com/en/")
 {
 	// HTML5Rocks but not article
-	window.alert("You are on html5rocks, Now Please choose an article");
+	window.alert("You are on html5rocks, Now Please choose an article to print");
 } else {
 	// Some website other than HTML5Rocks
 	window.alert("This extension only prettyfies html5rocks Please visit http://www.html5rocks.com");
--- README.md
+++ README.md
@@ -1,37 +1,38 @@
-[Welcome to PrettyPrintHTML5Rocks!](https://travis-ci.org/adobe/brackets)
+[Welcome to PrettyPrintHTML5ROCKS!](https://travis-ci.org/adobe/brackets)
 -------------------
 
-This is an early version of PrettyPrintHTML5Rocks, a Google Chrome extension built to *print* the articles from http://www.html5rocks.com 
+This is an early version of PrettyPrintHTML5ROCKS, a Google Chrome extension built to *print* the articles from http://www.html5rocks.com 
 
-###Why PrettyPrintHTML5ROcks?
+###Why PrettyPrintHTML5ROCKS?
 
 * **Paper should be used effectively.** Instead of wasting space on the paper you print your articles, this extension enable you to use the space effectively.
 * **Saves money and clutter.** By saving paper; you are not just helping our environment by keeping the count of dead trees to minimum, but it also saves you money and clutter. The less paper you use, the less you have to pay for it, and the lesser no. of pages will be there for you to take care of. All of the data that you care about will be in just a few pages, so you won't have to shuffle through a large pile to find that one small peice of reference you remember from when you read the article.
 
-How to install and use PrettyPrintHTML5Rocks
+How to install and use PrettyPrintHTML5ROCKS
 -------------------------------
 
 
 #### Google Chrome Web Store.
 
-To start using PrettyPrintHTML5Rocks, go to chrome web store and search for PrettyPrintHTML5Rocks or HTML5Rocks, and install the extension from the results. Alternatively you can go to the [extension page](http://www.ishanatmuz.wordpress.com) and install it.
+To start using PrettyPrintHTML5ROCKS, go to chrome web store and search for PrettyPrintHTML5ROCKS or HTML5Rocks, and install the extension from the results. Alternatively you can go to the [extension page](http://www.ishanatmuz.wordpress.com) and install it.
 
 
 #### Usage
 
-To use the extension. Go to any article on <http://www.html5rocks.com> which you want to print. Click on the PrettyPrintHTML5Rocks<icon here> icon on the top right hand side along with the menu. The webpage would have been modified and the print dialog will be opened for you print the article. You can save the article into PDF format too for printing later. Google Chrome supports saving any print task as a PDF.
+To use the extension. Go to any article on <http://www.html5rocks.com> which you want to print. Click on the PrettyPrintHTML5ROCKS<icon here> icon on the top right hand side along with the menu. The webpage would have been modified and the print dialog will be opened for you print the article. Clicking again on the icon will set the article back to original view.
+You can save the article into PDF format too for printing later. Google Chrome supports saving any print task as a PDF.
 
 #### Need help?
 
-Having problems Installing [PrettyPrintHTML5Rocks](http://www.ishanatmuz.wordpress.com) or printing or saving articles, just email me with 'PrettyPrintHTML5Rocks' as subject at ishanatmuzaffarpur@gmail.com.
+Having problems Installing [PrettyPrintHTML5ROCKS](http://www.ishanatmuz.wordpress.com) or printing or saving articles, just email me with 'PrettyPrintHTML5Rocks' as subject at ishanatmuzaffarpur@gmail.com.
 
 
-####Helping PrettyPrintHTML5Rocks
+####Helping PrettyPrintHTML5ROCKS
 ----------------
 
 #### Extension not working!
 
-Email me to notify that extension is not working with 'PrettyPrintHTML5Rocks' as subject at 'ishanatmuzaffarpur@gmail.com'. Do not forget to include the URL for which the error occurred.
+Email me to notify that extension is not working with 'PrettyPrintHTML5ROCKS' as subject at 'ishanatmuzaffarpur@gmail.com'. Do not forget to include the URL for which the error occurred.
 If the page is getting printed but some item is omitted which shouldn't be, save the print result as a PDF file and attach it with the email.
 
 
