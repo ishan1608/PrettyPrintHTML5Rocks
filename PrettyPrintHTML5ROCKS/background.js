6e2a3fc498e822b372a5b62e34ff21283576a28b
Ishan <ishanatmuzaffarpur@gmail.com>
2014-01-24 19:35:22 +0530

    still not a perfect solution

--- PrettyPrintHTML5ROCKS/background.js
+++ PrettyPrintHTML5ROCKS/background.js
@@ -1,10 +1,63 @@
 // Respond to the click on extension Icon
 chrome.browserAction.onClicked.addListener(function (tab) {
     'use strict';
+    
+    /*
+    // Context of jquery and print.js is not same.
+    function loadExtension(callback) {
+        chrome.tabs.executeScript({
+            file: 'thirdParty/jquery-2.0.3.js'
+        });
+        // callback.call(this);
+        // callback.bind(this);
+        callback.apply(this);
+        // callback();
+    }
+    
+    function runPrint() {
+        chrome.tabs.executeScript({
+            file: 'print.js'
+        });
+    }
+    
+    loadExtension(runPrint);
+    */
+    
+    /*
+    // This method doesn't works
+    chrome.tabs.executeScript({
+        file: 'thirdParty/jquery-2.0.3.js'
+    }).chrome.tabs.executeScript({
+        file: 'print.js'
+    });
+    */
+    
+    /*
+    // This method solves asynchronousity but causes jQuery to be not available to my script.
+    // I have NO idea why
+    function loadExtension(callback) {
+        chrome.tabs.executeScript({
+            file: 'thirdParty/jquery-2.0.3.js'
+        });
+        callback();
+    }
+    
+    function runPrint() {
+        chrome.tabs.executeScript({
+            file: 'print.js'
+        });
+    }
+    
+    loadExtension(runPrint);
+    */
+    
+    /**/
+    // Old method asynchronousity problem
 	chrome.tabs.executeScript({
         file: 'thirdParty/jquery-2.0.3.js'
 	});
     chrome.tabs.executeScript({
 		file: 'print.js'
 	});
+    /**/
 });
